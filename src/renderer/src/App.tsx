4ec125de808819c3541a892add0e53b17a22db5b
Pham Thanh Trung <adamward459@gmail.com>
2026-03-31 19:40:03 +0700

    init: electron-vite + react-ts + tailwind v4

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+function App(): React.JSX.Element {
+  return (
+    <div className="flex items-center justify-center h-screen bg-neutral-950 text-white">
+      <h1 className="text-2xl font-semibold">Differ</h1>
+    </div>
+  );
+}
+
+export default App;
